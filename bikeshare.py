eb9aaa0ae136ce7dbb284b31e5b902fb053a666d
Hind Alharbi <hind.a.alharbi@hotmail.com>
2019-11-11 17:29:51 +0300

    Modify the code

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input("\nWould you like to see data for Chicago, New York, or Washington?\n").lower()
     while(True):
-        if(city == 'chicago' or city == 'new york' or city == 'washington' or city == 'all'):
+          if (city in CITY_DATA.keys()):
             break
         else:
             city = input('Enter Correct city: ').lower()
